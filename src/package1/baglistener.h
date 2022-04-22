deec14c4d4a3eecd4a57e58e85abc9efd7f0596e
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2022-04-22 18:58:49 +0200

    Reversed commit

---
+++ src/package1/baglistener.h
@@ -0,0 +1,14 @@
+//
+// Created by Lorenzo Nebolosi on 22/04/22.
+//
+
+#ifndef PROJECT_BAGLISTENER_H
+#define PROJECT_BAGLISTENER_H
+
+
+class baglistener {
+
+};
+
+
+#endif //PROJECT_BAGLISTENER_H
