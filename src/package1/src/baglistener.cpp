ebbd76ec1207c2858778d4f5be93351c5e329825
Daniele Cicala <10630561@polimi.it>
2022-04-22 23:17:46 +0200

    created RPM.msg custom messagge and added main in baglistener.cpp

--- src/package1/src/baglistener.cpp
+++ src/package1/src/baglistener.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Lorenzo Nebolosi on 22/04/22.
 //
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+    return 0;
+}
