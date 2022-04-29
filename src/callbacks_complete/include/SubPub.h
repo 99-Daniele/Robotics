d37535e3582b252b005444898f5efbcbeb31e031
rosatele <elena.rosato@mail.polimi.it>
2022-04-29 17:58:24 +0200

    created SubPub class now velocity.cpp also publish on wheels.h

---
+++ src/callbacks_complete/include/SubPub.h
@@ -0,0 +1,23 @@
+#ifndef ROBOTICS_SUBPUB_H
+#define ROBOTICS_SUBPUB_H
+
+#include "ros/ros.h"
+#include "string"
+
+template<typename SubscribeT,typename PublishT>
+class SubPub{
+public:
+    SubPub() {}
+    SubPub(std::string SubscribeTopicName,std::string PublishTopicName, int queueSize){
+        subscriberObject=n.subscribe<SubscribeT>(SubscribeTopicName,queueSize,&SubPub::subscriberCallback,this);
+        publisherObject=n.advertise<PublishT>(PublishTopicName,queueSize);
+    }
+    void subscriberCallback(const typename SubscribeT::ConstPtr& recievedMsg);
+
+protected:
+    ros::Subscriber subscriberObject;
+    ros::Publisher publisherObject;
+    ros::NodeHandle n;
+};
+
+#endif //ROBOTICS_SUBPUB_H
--- src/callbacks_complete/src/velocity.cpp
+++ src/callbacks_complete/src/velocity.cpp
@@ -2,19 +2,20 @@
 #include "geometry_msgs/TwistStamped.h"
 #include "callbacks_complete/Subscriber.h"
 #include "callbacks_complete/RPM.h"
+#include "SubPub.h"
 
-
-void velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+template<>
+void SubPub<geometry_msgs::TwistStamped,callbacks_complete::RPM>::subscriberCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
 
     ROS_INFO("Linear velocity: x = %f, y = %f, z = %f", msg->twist.linear.x, msg->twist.linear.y, msg->twist.linear.z);
     ROS_INFO("Angular velocity: x = %f, y = %f, z = %f", msg->twist.angular.x, msg->twist.angular.y, msg->twist.angular.z);
     //float w0,w1,w2,w3;
     Subscriber s;
-
+/*
     ros::NodeHandle n;
 
     ros:: Publisher wheels_rpm_pub = n.advertise<callbacks_complete::RPM>("wheels_rpm", 1000);
-
+*/
     callbacks_complete::RPM wheel_speed;
     wheel_speed.rpm_fl=1/s.r*(msg->twist.linear.x-msg->twist.linear.y-(s.l+s.w)*msg->twist.angular.z);
     wheel_speed.rpm_fr=1/s.r*(msg->twist.linear.x+msg->twist.linear.y+(s.l+s.w)*msg->twist.angular.z);
@@ -22,20 +23,22 @@ void velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
     wheel_speed.rpm_rr=1/s.r*(msg->twist.linear.x-msg->twist.linear.y+(s.l+s.w)*msg->twist.angular.z);
 
     ROS_INFO("fl: %f, fr: %f, rl: %f, rr: %f", wheel_speed.rpm_fl,wheel_speed.rpm_fr,wheel_speed.rpm_rl,wheel_speed.rpm_rr);
-    wheels_rpm_pub.publish(wheel_speed);
+    publisherObject.publish(wheel_speed);
 
 
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "listener");
-    ros::NodeHandle n;
+    ros::init(argc, argv, "velocity");
+
+    SubPub<geometry_msgs::TwistStamped,callbacks_complete::RPM>velocity("cmd_vel","wheels_rpm",1000);
+   /* ros::NodeHandle n;
 
     ros::Subscriber sub_chatter = n.subscribe("cmd_vel", 1000, velocityCallback);
     ROS_INFO("prova se funziona il nodo");
 
     //ros:: Publisher wheels_rpm_pub = n.advertise<callbacks_complete::RPM>("wheels_rpm", 1000);
-
+*/
     ros::spin();
 
     return 0;
