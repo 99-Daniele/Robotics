ae7c25174ae28c6e340792746fd62d21d468f600
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2022-05-01 14:41:22 +0200

    Edited gitignore to ignore all the devel and build files

--- build/callbacks_complete/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lorenzonebolosi/Documents/project/robotics/src/callbacks_complete
-# Build directory: /home/lorenzonebolosi/Documents/project/robotics/build/callbacks_complete
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/prova_classes/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lsubscriber_class".split(';') if "-lsubscriber_class" != "" else []
-PROJECT_NAME = "callbacks_complete"
-PROJECT_SPACE_DIR = "/home/lorenzonebolosi/Documents/project/robotics/install"
-PROJECT_VERSION = "0.0.0"
